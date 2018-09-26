4afb822f2b00b6fe2b9c0fb8cd78af11a65fc942
Carly Schwichtenberg <cjschwichtenberg@gmail.com>
2018-09-25 20:08:36 -0400

    Adding Admin Auth

--- brewers-mark/server.js
+++ brewers-mark/server.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require('morgan');
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const routes = require("./routeservers");
+
 const app = express();
+
 const PORT = process.env.PORT || 3001;
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+//use sessions for tracking logins
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'drink it up',
+  resave: true,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+// Use body-parser for handling form submissions
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -15,8 +39,30 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// connect to Mongo Database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/adminAuthDB", { useNewUrlParser: true });
+var db = mongoose.connection;
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Mongo DB Connected")
+});
+
+// catch 404 and forward to error handler if neccessary
+// app.use(function(req, res, next) {
+//   var err = new Error('File Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler-define as the last app.use callback
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 // Start the API server
 app.listen(PORT, function() {
--- models/favlist.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var FavList = sequelize.define("FavList", {
-    userID: {type : DataTypes.STRING, primaryKey : true },
-    favorites: DataTypes.TEXT
-  });
-  return FavList;
-};
