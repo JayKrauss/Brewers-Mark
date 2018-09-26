657bfd3aa9b4b16cfdaa87e9a63da811828ab144
Carly Schwichtenberg <cjschwichtenberg@gmail.com>
2018-09-25 21:43:03 -0400

    Adding admin branch

--- brewers-mark/routes/api/index.js
+++ brewers-mark/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const adminRoutes = require("./admin");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/admin", adminRoutes);
 
 module.exports = router;
