ac6bb6d4f3c2913caf9f7fe515f9ed12f20272da
Jay Krauss <paxformatus@gmail.com>
2018-09-29 10:31:55 -0400

    IPA animation

---
+++ brewers-mark/client/src/components/Bottles/IPAmove.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './bottles.css';
+
+function IPAmove(){
+    document.getElementById("IPAchainsign").classList.add("IPAmove");
+  };
+  
+  export default IPAmove;
