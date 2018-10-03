419ee0101873b4d5cfcc38a21c500329e6e5b8e3
Jay Krauss <paxformatus@gmail.com>
2018-10-02 20:44:16 -0400

    Survey page, updated background for home, added routing to taps and routing for survey

--- brewers-mark/client/src/App.js
+++ brewers-mark/client/src/App.js
@@ -9,6 +9,7 @@ import Home from './components/Home/home.js'
 import Glass from './components/Glass/Glass.js'
 import BottlesNav from './components/Bottles/bottlesexport.js'
 import BeerList from './components/BeerList/tabsexport'
+import Survey from './components/Survey/survey.js'
 
 const App = () => (
   <Router>
@@ -18,6 +19,7 @@ const App = () => (
         <Route exact path="/glass" component={Glass} />
         <Route exact path="/IPA" component={BottlesNav} />
         <Route exact path="/beerlist" component={BeerList} />
+        <Route exact path="/survey" component={Survey} />
       </Switch>
 </div>
   </Router>
