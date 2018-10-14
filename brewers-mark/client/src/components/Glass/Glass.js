65ed1eacd5c001fb1f5f0122d6940474752091e8
Jay Krauss <paxformatus@gmail.com>
2018-10-14 19:48:16 -0400

    Adjusted CSS on glass and beerlist

--- brewers-mark/client/src/components/Glass/Glass.js
+++ brewers-mark/client/src/components/Glass/Glass.js
@@ -15,6 +15,7 @@ class Glass extends React.Component {
 
   componentDidMount() {
     $('body').css('background-image', 'url(./glassback.png)')
+    
     pourBeer();
     signmove();
     namemove();
@@ -29,6 +30,52 @@ class Glass extends React.Component {
       console.log("inside else")
       beerAPI.getById(beerId)
         .then(res => this.setState({ beer: res.data[0] }));
+
+    let EBC;
+
+      let IPA ='IPA';
+      let sour = 'Sour';
+      let lager = 'Lager';
+      let stout = 'Stout';
+
+      if (this.state.beer.beer_style.includes(IPA)){
+        EBC = 13
+      }
+      else if (this.state.beer.beer_style.includes(sour)){
+        EBC = 25
+      }
+      else if (this.state.beer.beer_style.includes(lager)){
+        EBC = 29
+      }
+      else if (this.state.beer.beer_style.includes(stout)){
+        EBC = 45
+      }  
+
+    function beerColor() {
+      
+        if (EBC <= 11) {
+          document.getElementById("beer").style.background="linear-gradient(white, rgb(184, 123, 28, .95),rgb(184, 123, 28, .95),rgb(184, 123, 28, .95), rgb(184, 123, 28, .95),rgb(184, 123, 28, .95))";
+          document.getElementById("pour").style.background="linear-gradient(rgb(184, 123, 28), rgb(184, 123, 28),white)";
+        }
+        else if ((EBC > 11) && (EBC <= 19)) {
+          document.getElementById("beer").style.background="linear-gradient(white, 	rgb(191, 136, 21, .95), 	rgb(191, 136, 21, .95), 	rgb(191, 136, 21, .95), 	rgb(191, 136, 21, .95),	rgb(191, 136, 21, .95))";
+          document.getElementById("pour").style.background="linear-gradient(rgb(191, 136, 21, .95), 	rgb(191, 136, 21, .95), white)";
+        }
+        else if ((EBC >= 20) && (EBC <= 28)) {
+          document.getElementById("beer").style.background="linear-gradient(white, 	rgb(114, 46, 24, .95),	rgb(114, 46, 24),	rgb(114, 46, 24), 	rgb(114, 46, 24),	rgb(114, 46, 24))";
+          document.getElementById("pour").style.background="linear-gradient(rgb(114, 46, 24), 	rgb(114, 46, 24),white)";
+        }
+        else if ((EBC > 28) && (EBC <=36)) {
+          document.getElementById("beer").style.background="linear-gradient(white,rgb(55, 30, 17, .95),rgb(55, 30, 17, .95),rgb(55, 30, 17, .95),rgb(55, 30, 17, .95),rgb(55, 12, 30, .95))";
+          document.getElementById("pour").style.background="linear-gradient(#4b0c11,rgb(55, 12, 17), white)";
+        }
+        else if (EBC > 36){
+          document.getElementById("beer").style.background="linear-gradient(white, rgb(21, 9, 1, 1), rgb(21, 9, 1, 1), rgb(21, 9, 1, 1), rgb(21, 9, 1, 1), rgb(21, 9, 1, 1), rgb(21, 9, 1, 1))";
+          document.getElementById("pour").style.background="linear-gradient(rgb(21, 9, 1, 1), rgb(21, 9, 1, 1), white)";
+      }
+    }
+
+   beerColor();
     }
   }
 
@@ -39,16 +86,16 @@ class Glass extends React.Component {
           <div id='beername'>
             <img src={beername} />
             <center>
-              <span id='beertitle'><strong>{this.state.beer.beer_name}</strong></span>
+              <p id='beertitle'><strong>{this.state.beer.beer_name}</strong></p>
             </center>
           </div>
         </div>
         <div className='row'>
           <div className='col-3'>
             <div id='leftinfo'>
-              {this.state.beer.beer_description}
-              <p>Style: {this.state.beer.beer_style}</p>
-              <p>ABV: {this.state.beer.beer_abv}</p>
+            <div className='container' id='leftcont'>
+              <p id='glassdescription'>{this.state.beer.beer_description}</p>
+              </div>
             </div>
           </div>
           <div id='mainglass' className='col-6'>
@@ -61,10 +108,13 @@ class Glass extends React.Component {
           </div>
           <div className='col-3'>
             <div id='rightinfo'>
-
-              <h5>{this.state.beer.brewery_name}</h5>
-              <p>{this.state.beer.brewery_type}</p>
-              <p>Beer Rating: {this.state.beer.rating_score}</p>
+              <div className='container' id='rightcont'>
+              <h5 id='glassbrewery'>{this.state.beer.brewery_name}</h5>
+              <p id='glasstype'>{this.state.beer.brewery_type}</p>
+              <p id='glasstyle'>Style: {this.state.beer.beer_style}</p>
+              <p id='glassABV'>ABV: {this.state.beer.beer_abv}</p>
+              <p id='glassrating'>Beer Rating: {this.state.beer.rating_score}</p>
+              </div>
             </div>
           </div>
         </div>
