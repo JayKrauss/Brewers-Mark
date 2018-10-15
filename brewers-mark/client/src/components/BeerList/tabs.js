cfb1cc29ed8bdb1f10c40d88726fecacc9b50e01
Jay Krauss <paxformatus@gmail.com>
2018-10-15 17:01:52 -0400

    I was on master oops

--- brewers-mark/client/src/components/BeerList/tabs.js
+++ brewers-mark/client/src/components/BeerList/tabs.js
@@ -5,12 +5,14 @@ import bubbles from '../Glass/manipulation/bubbles.js'
 
 
 export const TabBtn = props => (
-  <button className='btn btn-default beertab' onClick={() => props.onClick({ bid: props.bid,})}>
-        <div>
-          <h4 className='tabsh4'>{props.title}</h4>
+
+  <button className='btn btn-default beerbtn' onClick={() => props.onClick({ bid: props.bid,})}>
+        <div className='beertab'>
+        <marquee behavior="scroll" direction="left" scrollamount="3" className='tabsh4'>{props.title}</marquee>
           <a rel="noreferrer noopener" target="_blank" href={props.href}></a>
         </div>
   </button>
+
 );
 
 export default TabBtn;
