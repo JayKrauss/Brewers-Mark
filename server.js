05c70ca008e14299e67c09aefab21b44a659c96a
Jay Krauss <paxformatus@gmail.com>
2018-10-16 16:51:40 -0400

    THE BIG ONE

--- server.js
+++ server.js
@@ -1,48 +1,73 @@
-require("dotenv").config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
-var db = require("./models");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const routes = require("./routes");
+const cookieParser = require('cookie-parser');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3001;
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// connect to Mongo Database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/brewersmarkbeers", { useNewUrlParser: true });
+var db = mongoose.connection;
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Mongo DB Connected")
 });
 
-module.exports = app;
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+app.use(cookieParser());
+
+//use sessions for tracking logins
+ app.set('trust proxy', 1) // trust first proxy
+
+
+app.use(session({
+  secret: 'drink it up',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { secure: true },
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+
+// catch 404 and forward to error handler if neccessary
+// app.use(function(req, res, next) {
+//   var err = new Error('File Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handler-define as the last app.use callback
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.json('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
